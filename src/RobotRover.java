66024e0eb9847672184ced1d86fef74d019b958d
habbala <jesper.blomqvist@hotmail.com>
2017-09-05 15:50:07 +0200

    Skelett Robot

--- src/RobotRover.java
+++ src/RobotRover.java
@@ -1,5 +1,30 @@
 public class RobotRover {
+    Position[] path;
+    Position currentPosition;
+    long timestamp;
+    Orientation orientation;
+
     public RobotRover(){
+        
+    }
+
+    public Position GetRobotPosition(){
+        return null;
+    }
+
+    public void MoveRobotToPosition(){
+
+    }
+
+    public void ReadLaser(){
+
+    }
+
+    public void SetPath(){
+
+    }
+
+    public void ReadResponse(){
         
     }
 }
